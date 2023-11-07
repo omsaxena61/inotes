21a68b3dd75a5d51c59d69f9ec2d12375eddfdd4
omsaxena61 <omsaxena61@gmail.com>
2023-11-07 13:10:46 +0530

    add to github

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017";
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI);
+  console.log("connected to mongo");
+};
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const  connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log("app4");
+})
+app.get('/tyu', (req, res) => {
+  res.send('pandit maderchod')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
