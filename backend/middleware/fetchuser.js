d062d88b0dda94066bd98a1584181eb80626599b
omsaxena61 <omsaxena61@gmail.com>
2023-11-09 22:17:42 +0530

    use middleware

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,21 @@
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "bhupendra jogi";
+  
+const fetchuser=(req,res,next)=>{
+      const token=req.header('auth-token');
+      if(!token)
+      {
+        res.status(401).send({error:"please authenticate using a valid token"})
+      }
+      try {
+        const data=jwt.verify(token,JWT_SECRET);
+      req.user=data.user;
+      next();
+        
+      } catch (error) {
+        res.send(error.message)
+      }
+      
+}
+
+module.exports=fetchuser;
