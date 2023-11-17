e430fa9c62f0b2b768fd146276825f5c38003e3d
omsaxena61 <omsaxena61@gmail.com>
2023-11-17 21:39:59 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+
+import About from './components/About';
+import Home from './components/home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React OM
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+     
+      <Switch>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          {/* <Route exact path="/users">
+            <Users />
+          </Route> */}
+           <Route exact path="/">
+            <Home/>
+          </Route>
+          
+        </Switch>
+        </Router>
+    </>
   );
 }
 
