2e54d37d51f02690b8de870bd9f485f1d45bb895
omsaxena61 <omsaxena61@gmail.com>
2023-11-23 18:10:42 +0530

    change

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useContext ,useEffect} from 'react'
 import noteContext from '../context/notes/noteContext'
-import NoteState from '../context/notes/NoteState'
+
 const About = () => {
   const a=useContext(noteContext)
   useEffect(()=>{
     a.update()
+    // eslint-disable-next-line
   },[])
   return (
     <div> this is About {a.state.name} and he is in class {a.state.class}</div>
